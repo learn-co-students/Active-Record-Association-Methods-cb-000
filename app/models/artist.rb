7fe021d1e9436d6f6d39df99297aed6047a5dd23
Jen Poole <contact.vandepool@gmail.com>
2018-02-23 03:53:24 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,23 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  # return the first song, from an array of the artist's songs
+  # def get_first_song
+  #   self.songs.first
+  # end
+
+  #return the genre of the artist's first saved song
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
+  #return the number of songs associated with the artist
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.size
   end
 
+  #return the number of genres associated with the artist
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.size
   end
 end
